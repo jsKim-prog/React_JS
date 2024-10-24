9c13ab78e64951817134c06ba66e3e0d001355ab
10-6\kkw <jscode77@gmail.com>
2024-10-24 12:55:39 +0900

    상품API 연결완료

--- frontend/jsmall/src/router/root.js
+++ frontend/jsmall/src/router/root.js
@@ -1,10 +1,13 @@
 import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import todoRouter from "./todoRouter";
+import productsRouter from "./productsRouter";
 const Loading = <div>Loading...</div>
 const Main = lazy(()=> import("../pages/MainPage"))
 const About = lazy(()=>import("../pages/AboutPage"))
 const TodoIndex = lazy(()=>import("../pages/todo/IndexPage"))
+
+const ProductsIndex = lazy(()=>import("../pages/products/IndexPage"))
 const root = createBrowserRouter([
     {
         path:"",
@@ -18,6 +21,11 @@ const root = createBrowserRouter([
         path:"todo",
         element: <Suspense fallback={Loading}><TodoIndex/></Suspense>,
         children: todoRouter()
+    },
+    {
+        path:"products",
+        element: <Suspense fallback={Loading}><ProductsIndex/></Suspense>,
+        children: productsRouter()
     }
 ])
 export default root;
